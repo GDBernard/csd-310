6689d7cfc3e887e5d14e8bbdc2163bff0009e2e1
GDBernard <gavin.d.bernard@gmail.com>
2023-01-31 02:41:55 -0500

    Mod 8.2+8.3
    Added completed code for Module 8.2 and 8.3, including screenshots.

--- module_8/Mod 8.2/mysql_test.py
+++ module_8/Mod 8.2/mysql_test.py
@@ -1,36 +1,25 @@
-""" 
-    Title: pysports_join_queries.py
-    Author: Professor Krasso
-    Date: 16 July 2020
-    Description: Test program for joining the player and team tables
-"""
+#Written By: Gavin Bernard
+#Date: 1/30/2023
 
-""" import statements """
 import mysql.connector
 from mysql.connector import errorcode
 
-""" database config object """
 config = {
-    "user": "pysports_user",
-    "password": "MySQL8IsGreat!",
+    "user": "root",
+    "password": "sqlpass159",
     "host": "127.0.0.1",
     "database": "pysports",
     "raise_on_warnings": True
 }
 
 try:
-    """ try/catch block for handling potential MySQL database errors """ 
-
-    db = mysql.connector.connect(**config) # connect to the pysports database 
+    db = mysql.connector.connect(**config)
     
-    # output the connection status 
     print("\n  Database user {} connected to MySQL on host {} with database {}".format(config["user"], config["host"], config["database"]))
 
     input("\n\n  Press any key to continue...")
 
 except mysql.connector.Error as err:
-    """ on error code """
-
     if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
         print("  The supplied username or password are invalid")
 
@@ -41,6 +30,4 @@ except mysql.connector.Error as err:
         print(err)
 
 finally:
-    """ close the connection to MySQL """
-
     db.close()
